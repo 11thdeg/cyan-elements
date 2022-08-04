fce22240cbfa35489cae64deb3680049633f095c
Ville Takanen <vitkukissa@gmail.com>
2022-08-04 14:36:37 +0300

    feat: theme and mode vars added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from './styles/charna-color-theme-vars.sass'
 export * from './charna-icon'
 export * from './charna-column'
 export * from './charna-app-bar'
---
+++ src/utils/onClassChange.ts
@@ -0,0 +1,14 @@
+export function onClassChange(element:HTMLElement, callback:CallableFunction) {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (
+        mutation.type === 'attributes' &&
+          mutation.attributeName === 'class'
+      ) {
+        callback(mutation.target);
+      }
+    });
+  });
+  observer.observe(element, { attributes: true })
+  return observer.disconnect;
+}
