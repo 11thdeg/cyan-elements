f29d96abfa3dfccb1b732560c1e917300a666e9a
Ville Takanen <vitkukissa@gmail.com>
2022-08-04 12:20:19 +0300

    feat: app-bar

---
+++ src/charna-app-bar.ts
@@ -0,0 +1,35 @@
+import { html, css, LitElement } from 'lit'
+import { customElement } from 'lit/decorators.js'
+
+@customElement('charna-app-bar')
+export class CharnaAppBar extends LitElement {
+  
+  static styles = css`
+    :host nav{
+      display: flex;
+      flex-direction: row;
+      gap: var(--charna-bar-gap);
+      margin: 0;
+      padding: 0;
+      padding-left: var(--charna-margin);
+      padding-right: var(--charna-margin);
+      height: 47px;
+      border-bottom: solid 1px #acc;
+      align-items: center;
+    }
+    ::slotted(h1) {
+      font-size: 10px // var(--charna-font-size-button);
+    }
+  `
+  render () {
+    return html`<nav class="app-bar">
+      <slot></slot>
+    </nav>`
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'charna-app-bar': CharnaAppBar
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './charna-icon'
 export * from './charna-column'
+export * from './charna-app-bar'
