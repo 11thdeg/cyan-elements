caf1c56ff0b90d17427e1a86a83afd6932ae2a38
Ville Takanen <vitkukissa@gmail.com>
2022-08-16 10:52:36 +0300

    feat: icon cleanup and icon to beta

--- src/components/cyan-icon.ts
+++ src/components/cyan-icon.ts
@@ -1,8 +1,10 @@
 import { html, css, LitElement } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
-import nounsFile from './assets/proprietary/icons/nouns.json'
-import { logError } from './utils/loghelpers'
-import { onClassChange } from './utils/onClassChange'
+import nounsFile from '../assets/proprietary/icons/nouns.json'
+import { logError } from '../utils/loghelpers'
+import { onClassChange } from '../utils/onClassChange'
+
+const nouns = nounsFile as Record<string, string>
 
 @customElement('cyan-icon')
 export class CyanIcon extends LitElement {
@@ -49,13 +51,14 @@ export class CyanIcon extends LitElement {
     this.noun = 'missing'
   }
 
+  lightModeDisconnect:CallableFunction|null = null
+  lightmode = document.body.classList.contains('cyan--mode--dark') ? 'dark' : 'light'
 
   connectedCallback(): void {
     super.connectedCallback()
     this.lightModeDisconnect = onClassChange(document.body, (el:Element) => {
       this.lightmode = el.classList.contains('cyan--mode--dark') ? 'dark' : 'light'
       this.requestUpdate()
-      // logDebug('lightmode:', this.lightmode, el.classList)
     })
   }
 
@@ -64,16 +67,13 @@ export class CyanIcon extends LitElement {
     if (this.lightModeDisconnect) this.lightModeDisconnect()
   }
 
-  lightModeDisconnect:CallableFunction|null = null
-  lightmode = document.body.classList.contains('cyan--mode--dark') ? 'dark' : 'light'
-
   render () {
-    const nouns = nounsFile as Record<string, string>
-    const iconFileName = nouns[this.noun] || nouns.missing
-    if (!iconFileName) logError(`Icon '${this.noun}' not found`) // log error if no icon found
-    const iconpath = '/proprietary/icons/' + this.lightmode + '/' + iconFileName
+    if(!nouns[this.noun]) logError(`Icon '${this.noun}' not found`)
     const classes = this.xsmall ? 'xsmall' : this.small ? 'small' : this.xlarge ? 'xlarge' : this.large ? 'large' : ''
-    return html`<img src="${iconpath}" alt="${this.noun}" class="${classes}"/>`
+    return html`<img
+      src="/proprietary/icons/${this.lightmode}/${nouns[this.noun]||nouns['missing']}"
+      alt="${this.noun}"
+      class="${classes}"/>`
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './styles/cyan-color-theme-vars.sass'
 export * from './styles/cyan-typography-vars.sass'
-export * from './cyan-icon'
+export * from './components/cyan-icon'
 export * from './cyan-app-bar'
 export * from './cyan-toggle'
 export * from './cyan-spacer'
