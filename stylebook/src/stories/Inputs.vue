1eb1169c2c7c51e8b2d3f27ba63b4704d87cb3f5
Ville Takanen <vitkukissa@gmail.com>
2022-08-22 09:54:22 +0300

    fix: textfield styling

--- stylebook/src/stories/Inputs.vue
+++ stylebook/src/stories/Inputs.vue
@@ -2,13 +2,19 @@
 
 </script>
 <template>
-  <article class="Column" id="Inputs">
+  <article class="column" id="Inputs">
     <section>
       <h1>Textfield</h1>
       <cyan-textfield
         value="Example value"
         label="Example label"
         placeholder="Example placeholder"></cyan-textfield>
+      <br/>
+      <cyan-textfield
+        value="Example error-value"
+        label="Example label"
+        placeholder="Example placeholder"
+        error="Example error"></cyan-textfield>
     </section>
   </article>
 </template>
