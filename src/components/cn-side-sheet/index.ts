039cd88cf99d9a2b489517f18f1492cdcdc109e3
Ville Takanen <ville.takanen@futurice.com>
2023-03-01 11:34:28 +0200

    fix: booklayout and side-sheet

--- src/components/cn-side-sheet/index.ts
+++ src/components/cn-side-sheet/index.ts
@@ -1,3 +1,4 @@
+import './overrides.sass'
 import { html, css } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import { CyanThemedElement } from '../../cyan-themed-element'
@@ -10,13 +11,15 @@ export class CyanSideSheet extends CyanThemedElement {
       position: fixed;
       top: 64px;
       right: 0;
-      width: 400px;
+      width: 320px;
       height: calc(100% - 64px);
       border-left: 1px solid var(--cn-border-color, #ccc);
       margin: 0;
       padding: 24px;
-      background-color: var(--cn-background-level-1, #eee);
-      border-radius: 24px 0 0 0px;
+      transform: translateX(100%);
+    }
+    :host([open]) {
+      transform: translateX(0);
     }`
 
   @property({ type: Boolean, reflect: true })
