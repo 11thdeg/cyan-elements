2b22329c8bc1857f67d7cdddf3bead1fa3fb9562
Ville Takanen <ville.takanen@futurice.com>
2023-03-14 15:00:43 +0200

    fix: cn-card supports all basic functionality

--- src/components/cn-card/index.ts
+++ src/components/cn-card/index.ts
@@ -2,6 +2,7 @@ export * from './theme.sass'
 import { html, css } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import { CyanThemedElement } from '../../cyan-themed-element'
+import { marked} from 'marked'
 
 @customElement('cn-card')
 export class CyanCard extends CyanThemedElement {
@@ -9,42 +10,70 @@ export class CyanCard extends CyanThemedElement {
     :host {
       display: block;
       border-radius: var(--cn-border-radius-card, 16px);
-    }
-    :host .cardHeader nav {
-      display: flex;
-      flex-direction: row;
-      align-items: flex-start;
-      justify-content: left;
-      gap: 8px;
+      container-type: inline-size;
       position: relative;
     }
     :host .cardContent {
-      padding: var(--cn-padding-block, 16px);
+      padding: 0;
+      margin: 0;
+      border-radius: var(--cn-border-radius-card, 16px);
+      max-height: 100cqw;
+      overflow: hidden;
+      position: relative;
     }
-    :host .cover {
-      display: block;
-      width: 100%;
+    :host .cardContent img {
+      width: 100cqw;
+      height: 57cqw;
       object-fit: cover;
       border-radius: 16px;
       position: relative;
-    }
-    :host .cover::before {
-      content: "";
       display: block;
+    }
+    /*:host .cardContent::after {
+      content: '';
       position: absolute;
       top: 0;
-      bottom: 0;
       left: 0;
-      right: 0;
-      z-index: 10;
-      background: linear-gradient(hsla(var(--chroma-primary-i-hsl), 0) 0%, hsla(var(--chroma-primary-i-hsl), 0.5) 50%, hsla(var(--chroma-primary-i-hsl), 0) 100%);
-    }
-    :host([cover]) .cardHeader nav {
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(
+        -59deg, 
+        hsla(65deg, 0%, 0%, 0.23) 40%,
+        hsla(65deg, 100%, 63%, 0.23) 70%,
+        hsla(202deg, 100%, 63%, 0.23) 100%
+      );
+      pointer-events: none;
+      z-index: 1;
+    }*/
+    :host .cardContent + .cardNoun {
       position: absolute;
-      bottom: 0;
-      right: 0;
-      max-height: 24px;
+      top: 12px;
+      left: 12px;
+      margin: 0;
+      padding: 0;
+      z-index: 2;
     }
+    :host h4 {
+      margin: 12px 16px;
+      padding: 0;
+      font-family: var(--cyan-font-family-headline-4);
+      font-weight: var(--cyan-font-weight-headline-4);
+      font-size: var(--cyan-font-size-headline-4);
+      line-height: var(--cyan-line-height-headline-4);
+      letter-spacing: var(--cyan-letter-spacing-headline-4);
+      color: var(--cn-color-heading-card);
+    }
+    :host .snippet {
+      padding: 0;
+      margin: 0 16px;
+      font-family: var(--cyan-font-family-body-2);
+      font-weight: var(--cyan-font-weight-body-2);
+      font-size: var(--cyan-font-size-body-2);
+      line-height: var(--cyan-line-height-body-2);
+      letter-spacing: var(--cyan-letter-spacing-body-2);
+      color: var(--cyan-color-medium);
+    }
+    
     :host([elevation="1"]) {
       background-color: var(--cn-background-level-1, cyan);
     }
@@ -57,16 +86,22 @@ export class CyanCard extends CyanThemedElement {
     :host([elevation="4"]) {
       background-color: var(--cn-background-level-1, cyan);
     }
+    nav {
+      box-sizing: border-box;
+      padding: 12px 16px;
+      width: 100cqw;
+    }
   `
 
   @property({ type: Number, reflect: true }) elevation = 1
 
-  @property({ type: String, reflect: true }) cover = undefined
+  @property({ type: String, reflect: true }) noun = ''
 
-  // this is needed for type safety for html`<img src="${this.coverImageURL}" alt="" class="cover" />`
-  get coverImageURL () {
-    return this.cover || ''
-  }
+  @property({ type: String, reflect: true }) title = ''
+
+  @property({ type: String, reflect: true }) snippet = ''
+
+  @property({ type: String, reflect: true }) cover = ''
 
   connectedCallback(): void {
     super.connectedCallback()
@@ -74,18 +109,22 @@ export class CyanCard extends CyanThemedElement {
   }
 
   render () {
+    const snp = document.createElement('div');
+    snp.insertAdjacentHTML('beforeend', marked(this.snippet));
+    snp.classList.add('snippet');
     return html`
-      <div class="cardHeader">
-        ${this.cover ? html`<img src="${this.coverImageURL}" alt="" class="cover" />` : ''}
-        <nav>
-          <slot name="noun"></slot>
-          <slot name="title"></slot>
-        </nav>
-      </div>
-      <div class="cardContent">
-        <slot></slot>
-      </div>
-      <nav>
+      ${this.cover ? html`<div class="cardContent">
+        <img src=${this.cover} alt="" aria-hidden="true" />
+      </div>` : html`
+      <slot></slot>
+      `}   
+      ${this.noun ? html`<cyan-icon noun=${this.noun} class="cardNoun" large></cyan-icon>` : ''}
+
+      ${this.title ? html`<h4 class="downscaled">${this.title}</h4>` : ''}
+      
+      ${this.snippet ? html`${snp}` : ''}
+      
+      <nav class="cardBottomActions">
         <slot name="actions"></slot>
       </nav>
     `
